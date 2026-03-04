3a375fb33c65f3264de2371d48021ca26deb8780
Augusto Borrello <tua_email@esempio.com>
2026-03-04 19:07:15 +0100

    Selfwork PHP 01

---
+++ exs.1/ex1.php
@@ -0,0 +1,44 @@
+<?php
+
+// 1. Integer (Intero)
+$Integer = 42; // Un numero intero
+
+// 2. Float (Decimale)
+$numeroDecimale = 3.14159; // Un numero con la virgola
+
+// 3. String (Testo)
+$messaggio = "Ciao mondo in PHP!"; // Una sequenza di caratteri
+
+// 4. Boolean (Booleano)
+$boolean = true; // Un valore vero o falso (true/false)
+
+  var_dump("$Integer"); 
+  var_dump("$numeroDecimale");
+  var_dump("$messaggio");
+  var_dump("$boolean");
+
+?>
+
+
+
+
+<?php 
+// Definizione di costanti in PHP
+
+const intero = 10;
+
+const decimale = 5.5;
+
+const testo = "Esempio di costante";
+
+const booleano = false;
+
+var_dump(intero);
+
+var_dump(decimale);
+
+var_dump(testo);
+
+var_dump(booleano);
+
+?>
